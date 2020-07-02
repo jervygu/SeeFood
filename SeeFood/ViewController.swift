e0b4c08ca9b3686bbb10f73685c90033e162fbbe
jervygu <jervygu@gmail.com>
2020-07-02 13:32:57 +0800

    Complete Module 46, SeeFood App. 6. Getting Image Recognition Results Back from Our V3 Model

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -28,9 +28,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            detect(image: ciImage)
+            
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed.")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+//            print(results) // burger or not burger
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
         }
         
-        imagePicker.dismiss(animated: true, completion: nil)
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print("Handler error, \(error)")
+        }
     }
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
